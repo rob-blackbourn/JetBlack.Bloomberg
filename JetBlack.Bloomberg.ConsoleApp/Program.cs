46f4a0f428d13c2505f7d9e705e9b14619933bf0
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-10-28 08:22:09 +0000

    Updated for changes to library.

--- JetBlack.Bloomberg.ConsoleApp/Program.cs
+++ JetBlack.Bloomberg.ConsoleApp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
@@ -22,7 +21,7 @@ namespace JetBlack.Bloomberg.ConsoleApp
             {
                 ServerHost = "192.168.0.1", // Your server ip address
                 ServerPort = 8194, // Your server port
-                AuthenticationOptions = "AuthenticationMode=APPLICATION_ONLY;ApplicationAuthenticationType=APPNAME_AND_KEY;ApplicationName=XXXXXX", // Your server name
+                AuthenticationOptions = "AuthenticationMode=APPLICATION_ONLY;ApplicationAuthenticationType=APPNAME_AND_KEY;ApplicationName=XXXXXX", // Your application name
                 AutoRestartOnDisconnection = true
             };
 
@@ -36,12 +35,7 @@ namespace JetBlack.Bloomberg.ConsoleApp
 
             _bloomberg.AdminStatus += OnAdminStatus;
             _bloomberg.AuthenticationStatus += OnAuthenticationStatus;
-            _bloomberg.InitialisationStatus += (sender, eventArgs) =>
-            {
-                if (eventArgs.Args)
-                    _bloomberg.Request(IntradayBarRequest.Create("VOD LN Equity", new DateTime(2015, 10, 16, 9, 0, 0), new DateTime(2015, 10, 16, 12, 00, 0), EventType.BID, 60))
-                        .Done(intradayBarResponse => Console.WriteLine("{0}: [{1}]", intradayBarResponse.Ticker, string.Join(",", intradayBarResponse.IntradayBars.Select(intradayBar => string.Format("Open={0}, High={1}, Low={2}, Close={3}, Volume={4}, NumEvents={5}", intradayBar.Open, intradayBar.High, intradayBar.Low, intradayBar.Close, intradayBar.Volume, intradayBar.NumEvents)))));
-            };
+            _bloomberg.InitialisationStatus += OnInitialisationStatus;
 
             _bloomberg.StartAsync();
 
@@ -56,103 +50,109 @@ namespace JetBlack.Bloomberg.ConsoleApp
 
         private static void OnAuthenticationStatus(object sender, EventArgs<bool> eventArgs)
         {
-            Debug.Print("OnAuthenticationStatus: {0}", eventArgs);
+            Console.WriteLine("OnAuthenticationStatus: {0}", eventArgs);
         }
 
         private static void OnInitialisationStatus(object sender, EventArgs<bool> eventArgs)
         {
-            Debug.Print("OnInitialisationStatus: {0}", eventArgs);
+            Console.WriteLine("OnInitialisationStatus: {0}", eventArgs);
 
             if (eventArgs.Args)
             {
-                _bloomberg.ToObservable(new[] { "IBM US Equity" }, new[] { "BID", "ASK" })
+                _bloomberg.ToObservable(new[] { new SubscriptionRequest("VOD LN Equity", new[] { "BID", "ASK" }) })
                     .ObserveOn(TaskPoolScheduler.Default)
-                    .Subscribe(tickerData => Console.WriteLine("{0} - [{1}]", tickerData.Ticker, string.Join(",", tickerData.Data.Select(x => string.Format("{0}: {1}", x.Key, x.Value)))));
+                    .Subscribe(
+                        response =>
+                        {
+                            Console.WriteLine("Subscription Received.");
+                            Console.WriteLine("{0} - [{1}]", response.Ticker, string.Join(",", response.Data.Select(x => string.Format("{0}: {1}", x.Key, x.Value))));
+                        },
+                        error => Console.WriteLine("Subscription Error: {0}", error),
+                        () => Console.WriteLine("Subscription Completed."));
 
-                _bloomberg.Request(ReferenceDataRequest.Create(new[] {"VOD LN Equity"}, new[] {"PX_LAST"}))
-                    .Done(response => response.ReferenceData.Values.ForEach(tickerData => Console.WriteLine("{0} - [{1}]", tickerData.Ticker, string.Join(",", tickerData.Data.Select(x => string.Format("{0}: {1}", x.Key, x.Value))))));
+                _bloomberg.ToObservable(ReferenceDataRequest.Create(new[] { "VOD LN Equity", "TSCO LN Equity" }, new[] { "PX_LAST" }))
+                    .Subscribe(
+                        response =>
+                        {
+                            Console.WriteLine("Reference Data Received.");
+                            foreach (var item in response)
+                            {
+                                if (item.Value.IsLeft)
+                                    Console.WriteLine("Ticker={0}, Error={1}", item.Key, item.Value.Left);
+                                else
+                                    Console.WriteLine("{0} - [{1}]", item.Key, string.Join(",", item.Value.Right.Select(x => string.Format("{0}: {1}", x.Key, x.Value))));
+                            }
+                        },
+                        error => Console.WriteLine("Reference Data Error: {0}", error),
+                        () => Console.WriteLine("Reference Data Completed."));
 
-                //_bloomberg.Request(new SecurityEntitlementsRequest(new[] { "VOD LN Equity" }))
-                //    .Done(ShowEntitlements);
+                _bloomberg.ToObservable(new SecurityEntitlementsRequest(new[] { "VOD LN Equity", "IBM US Equity" }))
+                    .Subscribe(
+                        response =>
+                        {
+                            Console.WriteLine("Security Entitlements Received.");
+                            foreach (var item in response)
+                                Console.WriteLine("Ticker={0}, Entitlements=[{1}]", item.Key, string.Join(",", item.Value.EntitlementIds));
+                        },
+                        error => Console.WriteLine("Security Entitlements Error={0}", error),
+                        () => Console.WriteLine("Security Entitlements Completed."));
 
-                _bloomberg.Request(IntradayTickRequest.Create("VOD LN Equity", new[] { EventType.BID, EventType.ASK, EventType.TRADE }, new DateTime(2015, 10, 16, 9, 0, 0), new DateTime(2015, 10, 16, 9, 10, 0)))
-                    .Done(ShowIntradayTicks);
+                _bloomberg.ToObservable(IntradayTickRequest.Create("VOD LN Equity", new[] { EventType.BID, EventType.ASK, EventType.TRADE }, new DateTime(2015, 10, 16, 9, 0, 0), new DateTime(2015, 10, 16, 9, 10, 0)))
+                    .Subscribe(response =>
+                    {
+                        Console.WriteLine("Intraday Ticks Received.");
+                        Console.WriteLine("Ticker: {0}", response.Ticker);
+                        foreach (var intradayTick in response.IntraDayTicks)
+                            Console.WriteLine("  {0:yyyy-MM-dd HH:mm:ss.fff} - {1}: {2}", intradayTick.Time, intradayTick.EventType, intradayTick.Value);
+                    },
+                        error => Console.WriteLine("Intraday Tick Error: {0}", error),
+                        () => Console.WriteLine("Intraday Tick Completed."));
 
-                _bloomberg.Request(IntradayBarRequest.Create("VOD LN Equity", new DateTime(2015, 10, 16, 9, 0, 0), new DateTime(2015, 10, 16, 12, 00, 0), EventType.BID, 60))
-                    .Done(intradayBarResponse =>Console.WriteLine("{0}: [{1}]", intradayBarResponse.Ticker, string.Join(",", intradayBarResponse.IntradayBars.Select(intradayBar => string.Format("Open={0}, High={1}, Low={2}, Close={3}, Volume={4}, NumEvents={5}", intradayBar.Open, intradayBar.High, intradayBar.Low, intradayBar.Close, intradayBar.Volume, intradayBar.NumEvents)))));
+                _bloomberg.ToObservable(IntradayBarRequest.Create("VOD LN Equity", new DateTime(2015, 10, 16, 9, 0, 0), new DateTime(2015, 10, 16, 12, 00, 0), EventType.BID, 60))
+                    .Subscribe(
+                        intradayBarResponse =>
+                        {
+                            Console.WriteLine("Intraday Bar Received.");
+                            Console.WriteLine("{0}: [{1}]", intradayBarResponse.Ticker, string.Join(",", intradayBarResponse.IntradayBars.Select(intradayBar => string.Format("Open={0}, High={1}, Low={2}, Close={3}, Volume={4}, NumEvents={5}", intradayBar.Open, intradayBar.High, intradayBar.Low, intradayBar.Close, intradayBar.Volume, intradayBar.NumEvents))));
+                        },
+                        error => Console.WriteLine("Intraday Bar Error: {0}", error),
+                        () => Console.WriteLine("Intraday Bars complete"));
 
-                //_bloomberg.Request(HistoricalDataRequest.Create(new[] { "VOD LN Equity", "TSCO LN Equity" }, new [] {"BID", "ASK" }, new DateTime(2015, 10, 12, 9, 0, 0), new DateTime(2015, 10, 16, 17, 30, 0), PeriodicitySelection.DAILY))
-                //    .Done(ShowHistoricalData);
+                _bloomberg.ToObservable(HistoricalDataRequest.Create(new[] { "VOD LN Equity", "TSCO LN Equity" }, new[] { "PX_LAST" }, DateTime.Today.AddMonths(-2), DateTime.Today, PeriodicitySelection.DAILY))
+                    .Subscribe(
+                        response =>
+                        {
+                            Console.WriteLine("Historical Data Received");
+                            foreach (var item in response)
+                            {
+                                if (item.Value.IsLeft)
+                                {
+                                    Console.WriteLine("Ticker={0}, SecurityError={1}", item.Key, item.Value.Left);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ticker={0}", item.Key);
+                                    foreach (var dateAndFields in item.Value.Right)
+                                    {
+                                        Console.WriteLine("  Date: {0}", dateAndFields.Key);
+                                        foreach (var nameAndValue in dateAndFields.Value)
+                                            Console.WriteLine("    {0}: {1}", nameAndValue.Key, nameAndValue.Value);
+                                    }
+                                }
+                            }
+                        },
+                        error => Console.WriteLine("Historical Data Error: {0}", error),
+                        () => Console.WriteLine("Historical Data Completed."));
 
-                Debug.Print("It's asynchronous");
+                _bloomberg.RequestToken().Done(token => Console.WriteLine("Token={0}", token));
+
+                Console.WriteLine("It's asynchronous");
             }
-        }
-
-        private static void ShowHistoricalData(HistoricalDataResponse historicalDataResponse)
-        {
-            Debug.Print("ShowHistoricalData");
-
-            foreach (var historicalTickerData in historicalDataResponse.HistoricalTickerData.Values)
-            {
-                Debug.Print("Ticker={0}", historicalTickerData.Ticker);
-                foreach (var dateAndTickerData in historicalTickerData.Data)
-                {
-                    Debug.Print("  Date: {0}", dateAndTickerData.Key);
-                    foreach (var nameAndValue in dateAndTickerData.Value)
-                        Debug.Print("    {0}: {1}", nameAndValue.Key, nameAndValue.Value);
-                }
-            }
-        }
-
-        private static void ShowIntradayBars(IntradayBarResponse intradayBarResponse)
-        {
-            Debug.Print("ShowIntradayBars");
-            Debug.Print("Ticker: {0}", intradayBarResponse.Ticker);
-            foreach (var intradayBar in intradayBarResponse.IntradayBars)
-                Debug.Print("Open={0}, High={1}, Low={2}, Close={3}, Volume={4}, NumEvents={5}", intradayBar.Open, intradayBar.High, intradayBar.Low, intradayBar.Close, intradayBar.Volume, intradayBar.NumEvents);
-
-        }
-
-        private static void ShowIntradayTicks(IntradayTickResponse intradayTickResponse)
-        {
-            Debug.Print("ShowIntradayTicks");
-            Debug.Print("Ticker: {0}", intradayTickResponse.Ticker);
-            foreach (var intradayTick in intradayTickResponse.IntraDayTicks)
-                Debug.Print("  {0:yyyy-MM-dd HH:mm:ss.fff} - {1}: {2}", intradayTick.Time, intradayTick.EventType, intradayTick.Value);
-        }
-
-        private static void ShowEntitlements(SecurityEntitlementsResponse entitlementsByTicker)
-        {
-            foreach (var item in entitlementsByTicker.SecurityEntitlements.Values)
-                Debug.Print("Ticker={0}, Entitlements=[{1}]", item.Security, string.Join(",", item.EntitlementIds));
-        }
-
-        private static void DisplayReferenceData(ReferenceDataResponse referenceDataResponse)
-        {
-            foreach (var tickerData in referenceDataResponse.ReferenceData.Values)
-            {
-                Console.WriteLine("Ticker: {0}", tickerData.Ticker);
-                foreach (var field in tickerData.Data)
-                    Console.WriteLine("  {0}\t : {1}", field.Key, field.Value);
-            }
-        }
-
-        private static void DisplayTicks(TickerData tickerData)
-        {
-            Console.WriteLine("Ticker: {0}", tickerData.Ticker);
-            foreach (var field in tickerData.Data)
-                Console.WriteLine("\t{0} : {1}", field.Key, field.Value);
-            Console.WriteLine("{0} - [{1}]", tickerData.Ticker, string.Join(",", tickerData.Data.Select(x => string.Format("{0}: {1}", x.Key, x.Value))));
-        }
-
-        private static void DisplayError(Exception error)
-        {
-            Console.WriteLine("Subscription error: {0}", error);
         }
 
         private static void OnAdminStatus(object sender, EventArgs<AdminStatus> eventArgs)
         {
-            Debug.Print("OnAdminStatus: {0}", eventArgs.Args);
+            Console.WriteLine("OnAdminStatus: {0}", eventArgs.Args);
         }
     }
 
